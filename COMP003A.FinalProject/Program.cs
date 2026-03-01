8ef40007df6123c249d52c7d384b4bca1aca64b7
briannaordaz <briannaordaz5@gmail.com>
2026-03-01 01:59:11 -0800

    added main method and created a new class for registration with 25 fields

---
+++ COMP003A.FinalProject/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+//Event Registration System
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
---
+++ COMP003A.FinalProject/Registration.cs
@@ -0,0 +1,41 @@
+namespace COMP003A.FinalProject;
+
+public class Registration
+{
+    //Personal Information
+    public string FirstName; //Prompt Example: Enter first name:
+    public string LastName; //Enter last name:
+    public int Age; //Enter Age:
+    public string Email; //Enter Email:
+    public string PhoneNumber; //Enter Phone Number:
+    public string Address; //Enter Address:
+    public string City; //Enter City:
+    public string State; //Enter State:
+    public string Country; //Enter Country:
+    public string ZipCode; //Enter ZipCode:
+    
+    //Event Information
+    public string EventName; //Enter Event Name:
+    public DateTime EventDate; //Format: (mm/dd/yyyy)
+    public string Ticket; //Enter Ticket Type (General or VIP):
+    public string TicketID; //Enter Ticket ID:
+    public int SeatSection; //Enter Seat Section (1-300):
+    public char SeatRow; //Enter Seat Row (A-Z): 
+    public int SeatNumber; //Enter 
+    public DateTime RegistrationDate; //Format: (mm/dd/yyyy)
+    public char Attendence; //Have you attended the event? (y/n): 
+    public bool paid; //Have you paid for the ticket? (y/n)
+    
+    //Payment Information
+    public string PaymentMethod; //Enter Payment Method (cash or card):
+    public string CardHolder; //Enter CardHolder Name:
+    public string CardNumber; //Enter Card Number:
+    public string Expiration; //Enter Expiration Date:
+    public string SecurityCode; //Enter Security Code: 
+
+
+
+
+
+
+}
